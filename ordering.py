9aa3aea9cfb3468aaa5a79697e26e60a60182641
GiuliaClerici <giuliaclerici1@gmail.com>
2020-11-23 17:42:53 +0100

    Test on ordering - update after 2nd meeting

--- ordering.py
+++ ordering.py
@@ -5,12 +5,126 @@ import random
 k = 6 # numero delle azioni
 rounding = 5 # numero di cifre decimali a cui arrotondare
 means = np.empty(k) # array che conterrà le medie delle azioni, per ora vuoto
-policies = np.empty(k)
+policies = np.empty(k) # array che conterrà i reward delle policy
 temp = 0
 temp2 = 0
 
 for i in range(k): # per ogni azione
     means[i] = round(random.uniform(0,1), rounding) # genero un numero random tra o ed 1, arrotondato alla quinta cifra decimale e lo memorizzo nel vettore delle medie
+
+
+def checkOrder(a): # func. per controllare che l'array sia ordinato in ordine decrescente
+    return all(a[i] >= a[i + 1] for i in range(len(a) - 1))
+
+# calcolo il reward di ogni policy
+def rewardPolicies(p, m, k):
+    for i in range(k):
+        sum = 0
+        for j in range(i+1):
+            sum += m[j]
+        p[i] = round(sum / (i+1), rounding)
+    return p
+
+def medianPol(m, k):
+    mediane = np.empty(k)
+    mediane[0] = m[0]
+    for i in range(1, k):
+        mediane[i] = np.median(m[0:i+1])
+    return mediane
+
+means = np.sort(means)[::-1] # ordino le medie in modo decrescente
+
+print("medie ordinate: ", means)
+med =np.median(means)
+m = np.mean(means)
+
+
+# scambio due posizioni
+pos1 = random.randint(0, k-1)
+pos2 = random.randint(0, k-1)
+if pos1 == pos2:
+    pos2 = (pos2 + 1) % k
+temp = means[pos1]
+means[pos1] = means[pos2]
+means[pos2] = temp
+print("posizioni scambiate: ", pos1, " e ", pos2)
+means = [0.9, 0.8, 0.7, 0.2, 0.5, 0.1] # medie preimpostate per test
+#means = [0.61794, 0.84724, 0.54763, 0.47389, 0.46413, 0.21241]
+
+
+print("medie con avvenuto scambio: ", means)
+mediane = medianPol(means, k)
+print("mediane periodiche: ", mediane)
+policies = rewardPolicies(policies, means, k)
+print("reward delle policy: ", policies)
+
+
+checkM = checkOrder(means)
+checkP = checkOrder(policies)
+
+
+for i in range(k):
+    print("i: ", i)
+    for j in range(k-1):
+        print("j: ", j)
+        print(policies[j])
+        print(policies[j+1])
+        if policies[j] < policies[j+1]:
+            # swap
+            temp = policies[j]
+            policies[j] = policies[j+1]
+            policies[j+1] = temp
+            temp2 = means[j]
+            means[j] = means[j + 1]
+            means[j + 1] = temp2
+            #policies = rewardPolicies(policies, means, k)
+            print("means 2: ", means)
+            print("policies: ", policies)
+
+    #print("policies: ", policies)
+    print("means: ", means)
+    policies = rewardPolicies(policies, means, k)
+    print("policies: ", policies)
+
+
+#print("Però attenzione che i reward non sono ancora giusti, vanno ricalcolati secondo l'ordinamento nuovo.")
+#print("medie ordinate correttamente: ", means)
+#print("policy ordinate correttamente:", policies)
+
+'''
+def movePolMeans(policies, means, k):
+    for i in range(k):
+        succ = k-1 if i == k-1 else i + 1
+        if policies[i] < policies[succ]:
+            print("policies dentro: ", policies)
+            # scambio con precedente, sposto sia policy che media
+            # prima mi occupo della policy
+            temp = policies[i]
+            policies[i] = policies[succ]
+            policies[succ] = temp
+            # poi mi occupo della media
+            temp2 = means[i]
+            means[i] = means[succ]
+            means[succ] = temp2
+            policies = rewardPolicies(policies, means, k)
+    print("policy in move: ", policies)
+    print("medie in move: ", means)
+    for i in range(k-1, -1, -1):
+        prec = 0 if i == 0 else i - 1
+        if policies[i] > policies[prec]:
+            print("dentro succ")
+            # scambio con successiva, sposto sia policy che media
+            # prima mi occupo della policy
+            temp = policies[i]
+            policies[i] = policies[prec]
+            policies[prec] = temp
+            # poi mi occupo della media
+            temp2 = means[i]
+            means[i] = means[prec]
+            means[prec] = temp2
+            policies = rewardPolicies(policies, means, k)
+    return policies, means
+
 
 
 means = np.sort(means)[::-1] # ordino le medie in modo decrescente
@@ -26,50 +140,28 @@ temp = means[pos1]
 means[pos1] = means[pos2]
 means[pos2] = temp
 
-print("medie con avvenuto scambio: ", means)
 #means = [0.9, 0.2, 0.7, 0.5, 0.8, 0.1] # medie preimpostate per test
+#means = [0.61794, 0.84724, 0.54763, 0.47389, 0.46413, 0.21241]
 
-# calcolo il reward di ogni policy
-for i in range(k):
-    sum = 0
-    for j in range(i+1):
-        sum += means[j]
-    policies[i] = sum / (i+1)
+print("medie con avvenuto scambio: ", means)
+policies = rewardPolicies(policies, means, k)
 print("reward delle policy: ", policies)
 
-for i in range(k):
-    succ = k-1 if i == k-1 else i + 1
-    if policies[i] < policies[succ]:
-        # scambio con precedente, sposto sia policy che media
-        # prima mi occupo della policy
-        temp = policies[i]
-        policies[i] = policies[succ]
-        policies[succ] = temp
-        # poi mi occupo della media
-        temp2 = means[i]
-        means[i] = means[succ]
-        means[succ] = temp2
-for i in range(k-1, -1, -1):
-    prec = 0 if i == 0 else i - 1
-    if policies[i] > policies[prec]:
-        # scambio con successiva, sposto sia policy che media
-        # prima mi occupo della policy
-        temp = policies[i]
-        policies[i] = policies[prec]
-        policies[prec] = temp
-        # poi mi occupo della media
-        temp2 = means[i]
-        means[i] = means[prec]
-        means[prec] = temp2
+checkM = checkOrder(means)
+checkP = checkOrder(policies)
 
-print("Policy riordinate: ", policies)
-print("Però attenzione che i reward non sono ancora giusti, vanno ricalcolati secondo l'ordinamento nuovo.")
-print("medie ordinate correttamente: ", means)
+while (not checkM or not checkP):
+    policies, means = movePolMeans(policies, means, k)
+    print("Policy riordinate: ", policies)
+    print("Medie riordinate: ", means)
+    policies = rewardPolicies(policies, means, k)
+    print("Ricalcolo reward con ordinamento corretto: ", policies)
 
-# calcolo il reward di ogni policy con l'ordinamento corretto
-for i in range(k):
-    sum = 0
-    for j in range(i+1):
-        sum += means[j]
-    policies[i] = sum / (i+1)
-print("reward delle policy con ordinamento corretto: ", policies)
+    checkM = checkOrder(means)
+    checkP = checkOrder(policies)
+    print("checkM: ", checkM)
+    print("checkP: ", checkP)
+    print("not checkM: ", not checkM)
+    print("not checkP: ", not checkP)
+
+'''
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import numpy as np
 from armsMAB import Arms
 from BanditRanker import banditRanker
 from PiLow import  piLow
+from operator import itemgetter
 
 
 def main():
@@ -14,21 +15,80 @@ def main():
     #armsSet = {}
     #armsSet = [Arms(mean=i / 10, delay=1, gamma=0.5, state=0) for i in range(10, 0, -1)]
     #armsSet[9]._delay = 1
-    armsSet = [Arms(mean=random.uniform(0, 1), delay=i+1, gamma=0.5, state=0) for i in range(k)] # genero un'istanza di Arms (dunque un'azione) con media, parametro di delay, gamma e stato
-    #for x in range(10):
-    #    print(armsSet[x]._mean) # ricorda che qui la prima azione è 0, non 1
-    #    print(armsSet[x]._delay)
-    #    print(armsSet[x]._state)
+    armsSet = [Arms(mean=random.uniform(0, 1), delay=(i+1)%5 + 1, gamma=0.5, state=0) for i in range(k)] # genero un'istanza di Arms (dunque un'azione) con media, parametro di delay, gamma e stato
+    #means = np.empty(k)
+    #for x in range(k):
+        #means[x] = armsSet[x]._mean
+        #print(armsSet[x]._mean) # ricorda che qui la prima azione è 0, non 1
+        #print(armsSet[x]._delay)
+        #print(armsSet[x]._state)
+
+    armsSet.sort(key=lambda x: x._mean, reverse=True) # ordino le azioni in modo decrescente rispetto alla media _mean
+
+    # scambio due posizioni
+    pos1 = random.randint(0, k - 1)
+    pos2 = random.randint(0, k - 1)
+    if pos1 == pos2:
+        pos2 = (pos2 + 1) % k
+    temp = armsSet[pos1]
+    armsSet[pos1] = armsSet[pos2]
+    armsSet[pos2] = temp
+
+    # stampo le medie delle azioni ordinate in modo decrescente
+    for x in range(k):
+        print(armsSet[x]._mean)  # ricorda che qui la prima azione è 0, non 1
+        print(armsSet[x]._delay)
+
+    # calcolo reward delle policy senza delay
+    g = np.empty(k) # reward medi delle policy senza delay
+    gdelay = np.empty(k) # reward medi delle policy con delay
+
+    def rewardPolicies(g, m, k):
+        for i in range(k):
+            sum = 0
+            for j in range(i + 1):
+                sum += m[j]._mean
+            g[i] = round(sum / (i + 1), 5)
+        return g
+
+    g = rewardPolicies(g, armsSet, k)
+    print("g: ", g)
+
+    # calcolo reward delle policy con delay
+    # gioco ogni azione in modo da calibrare e avere tutti stati pari a 1 dunque attivi i delay
+    for i in range(k):
+        armsSet[i]._state = k - i
+
+
+    def rewardPoliciesDelay(gdelay, m, k):
+        for i in range(k):
+            t = 0
+            sum = 0
+            while(t <= i):
+                r, sample = m[t].sampleReward(rep_index=0)
+                for x in range(i):
+                    if x != t:
+                        m[x].updateState()
+                sum += r
+                t += 1
+            gdelay[i] = round(sum / (i + 1), 5)
+            sum = 0
+            t = 0
+        return gdelay
+
+    gdelay = rewardPoliciesDelay(gdelay, armsSet, k)
+    print("gdelay: ", gdelay)
+
+    # calcolo reward dopo campionamento
 
     #chiamo il primo algoritmo di ordinamento delle azioni
-    orderedArms = banditRanker(armsSet, k) # sistemare tuning di epsilon
-
-    for i in range(k):
-        print(orderedArms[i]._mean)
-        print(orderedArms[i]._delay)
-
+    #orderedArms = banditRanker(armsSet, k) # sistemare tuning di epsilon
+    #for i in range(k):
+    #    print(orderedArms[i]._mean)
+    #    print(orderedArms[i]._delay)
     #chiamo il secondo algoritmo ad eliminazione che restituisce l'insieme di policy ottime
-    piLow(orderedArms, k, horizon) # sistemare tuning di S nella costante C quando confronto i g(m)
+    #piLow(orderedArms, k, horizon) # sistemare tuning di S nella costante C quando confronto i g(m)
+
 
 
 
